938559284cfd2942899781fc4777ff243469d2d3
mazen <mazen.abono7tah@gmail.com>
2025-04-27 11:47:45 +0400

    config the client application

--- client-app/src/app/app.config.ts
+++ client-app/src/app/app.config.ts
@@ -1,8 +1,19 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, inject, provideZoneChangeDetection,provideApp } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { KeycloakService } from './utils/keycloak/keycloak.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+     provideRouter(routes),
+    provideHttpClient(),
+    provideAppInializer(()=>{
+      const initFn=((key:KeycloakService){
+        return  ()=>key.init();
+      })(inject(KeycloakService));
+      return initFn();
+    })
+  ]
 };
